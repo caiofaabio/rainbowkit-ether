16e05fe2fccff1649324de00b5a8b45d3653e525
Caio Fabio <faabio.caio@gmail.com>
2023-07-07 18:54:01 -0300

    feat: correct

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,12 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+import { mainnet, polygon, optimism, arbitrum, zora, bsc } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, zora],
+  [mainnet, bsc, polygon, optimism, arbitrum, zora],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
